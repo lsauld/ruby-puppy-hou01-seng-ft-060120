aceaebc489f44fe212e80dc21992c986525e31a9
lsauld <you@example.com>
2020-06-05 17:59:06 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+# Add your code hereclass Dog
+class Dog
+    @@all = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def Dog.all
+        @@all
+    end
+
+    def Dog.clear_all
+        @@all.clear()
+    end
+    
+    def Dog.print_all
+        @@all.each do |names|
+            puts names.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+end
