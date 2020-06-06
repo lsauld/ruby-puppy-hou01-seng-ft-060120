5cd6409b44d06db5510e012ee8d7c9786a5fc569
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-06-06 01:38:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+# Add your code herelass Dog
+    @@all = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def Dog.all
+        @@all
+    end
+
+    def Dog.clear_all
+        @@all.clear()
+    end
+    
+    def Dog.print_all
+        @@all.each do |names|
+            puts names.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+end
